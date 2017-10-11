6a105eb33068c1639d98533d39066fa8a64cbd4e
L115student <L115student@L115-08-043627.bitnet.wctc.edu>
2017-10-11 18:36:44 -0500

    Created LineItem Object

--- src/discountstrategyproject/DataObjects/LineItem.java
+++ src/discountstrategyproject/DataObjects/LineItem.java
@@ -4,7 +4,39 @@ public class LineItem {
     private String productId;
     private int quantity;
     
+    //-------------------//
+    //--- Constructor ---//
+    //-------------------//
     public LineItem(String productId, int quantity){
-        
+        setProductId(productId);
+        setQuantity(quantity);
     }
+    
+    //----------------------//
+    //--- Setter Methods ---//
+    //----------------------//
+    
+    public final void setProductId(String productId){
+        if(productId == null || !productId.matches("[A-Z]\\d{3}"))
+            throw new IllegalArgumentException("Product Id Must Match A### Format");
+        this.productId = productId;
+    }
+    
+    public final void setQuantity(int quantity){
+        if(quantity < 0) throw new IllegalArgumentException("Quantity May Not Be Negtivie");
+        this.quantity = quantity;
+    }
+    
+    //----------------------//
+    //--- Getter Methods ---//
+    //----------------------//
+
+    public String getProductId() {
+        return productId;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+    
 }
